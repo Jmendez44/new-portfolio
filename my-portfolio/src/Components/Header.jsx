000699a9db50748f2372dde31b9aea7c51a2b21a
jmendez44 <jenuel.mendez@gmail.com>
2019-06-24 01:34:26 -0400

    init react app

---
+++ my-portfolio/src/Components/Header.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <>
+        <div className="header-container">
+          <div className="about about-text">
+            <h1>Jenuel Mendez</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam assumenda voluptatem quam, tempore sint excepturi recusandae ullam deleniti voluptates, accusantium suscipit cumque error inventore eveniet quod quae animi, dicta vitae.</p>
+          </div>
+          <div className="about about-buttons">
+            <div className="btn work-btn">Work</div>
+            <div className="btn contact-btn">Contact</div>
+          </div>
+        </div>
+      </>
+     );
+  }
+}
+ 
+export default Header;
