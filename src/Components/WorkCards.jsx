c24267fe2fbfb4df98b47e543c855c85e3e702b4
jmendez44 <jenuel.mendez@gmail.com>
2019-07-18 17:14:02 -0400

    add social links

---
+++ src/Components/WorkCards.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Cards extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <>
+        
+      </>
+     );
+  }
+}
+ 
+export default Cards;
