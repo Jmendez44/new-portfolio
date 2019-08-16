833bbaeecb05adf1a99e69ae8ff8eb671191546e
jmendez44 <jenuel.mendez@gmail.com>
2019-08-16 04:30:51 -0400

    working on adding react-spring

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,24 +1,50 @@
-import React, { Component } from 'react';
-import './Header.css';
+import React, { Component } from "react";
+import { useSpring, animated } from "react-spring";
+import { Spring } from "react-spring/renderprops";
+import { interpolate } from "flubber" 
+// import { TimingAnimation } from 'react-spring/dist/addons'
+import "./Header.css";
 
 class Header extends Component {
-  state = {  }
-  render() { 
-    return ( 
+  state = { offset: 0 };
+  render() {
+    // const props = useSpring({
+    //   opacity: 1,
+    //   from: { opacity: 0 },
+    // })
+    const { offset } = this.state;
+
+    return (
       <>
         <div className="header-container" id="header">
           <div className="about about-text">
             <h1>Jay Mendez</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam assumenda voluptatem quam, tempore sint excepturi recusandae ullam deleniti voluptates, accusantium suscipit cumque error inventore eveniet quod quae animi, dicta vitae.</p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam
+              assumenda voluptatem quam, tempore sint excepturi recusandae ullam
+              deleniti voluptates, accusantium suscipit cumque error inventore
+              eveniet quod quae animi, dicta vitae.
+            </p>
           </div>
           <div className="about about-buttons">
-            <canvas id="c" width="800" height="250"/>
-
+            {/* <canvas id="c" width="800" height="250"/> */}
+            {/* <animated.h1 style={props}>hello</animated.h1> */}
+            <svg width="500" viewBox="0 0 23 23">
+              <g fill="transparent" stroke="black" strokeWidth="0.2">
+                <Spring from={{ x: 100 }} to={{ x: 0 }}>
+                  {props => (
+                    <svg strokeDashoffset={props.x}>
+                      <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+                    </svg>
+                  )}
+                </Spring>
+              </g>
+            </svg>
           </div>
         </div>
       </>
-     );
+    );
   }
 }
- 
+
 export default Header;
